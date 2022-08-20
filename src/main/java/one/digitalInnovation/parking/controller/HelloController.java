f32308d6c73e3c727bb476b44a0074e91302ea29
Silasoli <silas.oliveira@blockhub.builders>
2022-08-20 01:04:28 -0300

    Add estrutura inicial

---
+++ src/main/java/one/digitalInnovation/parking/controller/HelloController.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package one.digitalInnovation.parking.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author silas
+ */
+@RestController
+@RequestMapping("/")
+public class HelloController {
+    
+    @GetMapping
+    public String hello(){
+        return "Hello, Dev";
+    }
+    
+}
